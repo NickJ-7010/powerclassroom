bd5d0844c3cfc5a8b0479c124168d14cd940deae
Nick J <nickj7010@gmail.com>
2023-06-06 20:37:10 -0400

    update some html and perform better practices

--- index.js
+++ index.js
@@ -17,9 +17,18 @@ const server = http.createServer((req, res) => {
     if (req.url == '/favicon.ico') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'image/x-icon');
-        res.end(fs.readFileSync('assets/favicon.ico'));
+        res.end(fs.readFileSync('assets/static/favicon.ico'));
+    } else if (req.url == '/manifest.json') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(fs.readFileSync('manifest.json'));
+    } else if (req.url == '/robots.txt') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('');
     } else if (req.url.startsWith('/assets/') && fs.existsSync(decodeURI(req.url.slice(1)))) {
         res.statusCode = 200;
+        if (req.url.startsWith('/assets/static/')) res.setHeader('Cache-Control', 'max-age=31536000');
         res.setHeader('Content-Type', mimeTypes[decodeURI(req.url.slice(1)).split('.').pop()] ?? 'text/plain');
         res.end(fs.readFileSync(decodeURI(req.url.slice(1))));
     } else if (req.url == '/ping') {
